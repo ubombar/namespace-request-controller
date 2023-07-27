fe9134853b700341fb38737a5694a6d8f76a6dcf
ubombar <ufukbombar@gmail.com>
2023-07-27 10:42:48 +0200

    Add signals

--- cmd/namespace-request-controller/main.go
+++ cmd/namespace-request-controller/main.go
@@ -1,24 +1,73 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+	"time"
 
-	"github.com/ubombar/namespace-request-controller/pkg/apis/namespacerequest/v1alpha1"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"github.com/ubombar/namespace-request-controller/pkg/signals"
+	kubeinformers "k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/klog/v2"
+
+	// Uncomment the following line to load the gcp plugin (only required to authenticate against GKE clusters).
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+
+	controllerv1alpha1 "github.com/ubombar/namespace-request-controller/pkg/controllers/namespacerequest/v1alpha1"
+	clientset "github.com/ubombar/namespace-request-controller/pkg/generated/clientset/versioned"
+	informers "github.com/ubombar/namespace-request-controller/pkg/generated/informers/externalversions"
+)
+
+var (
+	masterURL  string
+	kubeconfig string
 )
 
 func main() {
-	fmt.Println("Test")
+	klog.InitFlags(nil)
+	flag.Parse()
 
-	a := v1alpha1.NamespaceRequest{
-		ObjectMeta: v1.ObjectMeta{
-			Name:      "test",
-			Namespace: "Tets",
-		},
-		Spec: v1alpha1.NamespaceRequestSpec{
-			NamespaceName: "test-namespace",
-		},
+	// set up signals so we handle the shutdown signal gracefully
+	ctx := signals.SetupSignalHandler()
+	logger := klog.FromContext(ctx)
+
+	cfg, err := clientcmd.BuildConfigFromFlags(masterURL, kubeconfig)
+	if err != nil {
+		logger.Error(err, "Error building kubeconfig")
+		klog.FlushAndExit(klog.ExitFlushTimeout, 1)
 	}
 
-	fmt.Printf("a: %v\n", a)
+	kubeClient, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		logger.Error(err, "Error building kubernetes clientset")
+		klog.FlushAndExit(klog.ExitFlushTimeout, 1)
+	}
+
+	exampleClient, err := clientset.NewForConfig(cfg)
+	if err != nil {
+		logger.Error(err, "Error building kubernetes clientset")
+		klog.FlushAndExit(klog.ExitFlushTimeout, 1)
+	}
+
+	kubeInformerFactory := kubeinformers.NewSharedInformerFactory(kubeClient, time.Second*30)
+	customInformerFactory := informers.NewSharedInformerFactory(exampleClient, time.Second*30)
+
+	controller := controllerv1alpha1.NewController(ctx, kubeClient, exampleClient,
+		kubeInformerFactory.Core().V1().Namespaces(),
+		customInformerFactory.Namespacerequest().V1alpha1().NamespaceRequests())
+
+	// notice that there is no need to run Start methods in a separate goroutine. (i.e. go kubeInformerFactory.Start(ctx.done())
+	// Start method is non-blocking and runs all registered informers in a dedicated goroutine.
+	kubeInformerFactory.Start(ctx.Done())
+	customInformerFactory.Start(ctx.Done())
+
+	if err = controller.Run(ctx, 2); err != nil {
+		logger.Error(err, "Error running controller")
+		klog.FlushAndExit(klog.ExitFlushTimeout, 1)
+	}
+}
+
+func init() {
+	flag.StringVar(&kubeconfig, "kubeconfig", "", "Path to a kubeconfig. Only required if out-of-cluster.")
+	flag.StringVar(&masterURL, "master", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
 }
